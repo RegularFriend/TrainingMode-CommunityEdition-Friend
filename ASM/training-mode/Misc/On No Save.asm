a77e8a49422bcb79de6920022036245e51356125
Nick Condron <ncondron42@gmail.com>
2025-04-20 11:56:04 -0400

    Simplify save setup

--- ASM/training-mode/Misc/On No Save.asm
+++ ASM/training-mode/Misc/On No Save.asm
@@ -6,48 +6,18 @@
     .set MEMCARD_NONE, 0xF
     .set MEMCARD_NONE2, 0xD
 
-    # Check if no memcard inserted
+    # Skip if we have save
     cmpwi r29, MEMCARD_HASSAVE
     beq Original
-    cmpwi r29, MEMCARD_NOSAVE
-    beq NoSave
-    cmpwi r29, MEMCARD_NONE
-    beq NoMemcard
-    cmpwi r29, MEMCARD_NONE2
-    beq NoMemcard
-
-NoSave:
-    bl InitSave
-    b Original
-
-NoMemcard:
-    bl InitSave
-
-    # Exit memcard think and disable saving
-    branch r12, 0x801b01ac
 
 InitSave:
     backup
-
-    # Get trophy data
-    lwz r3, MemcardData(r13)
-    addi r3, r3, 7376
-    # Set trophy count
-    li r4, 293
-    sth r4, 0x0(r3)
-    # Set individual trophies as unlocked
-    addi r3, r3, 4
-    li r4, 99
-    li r5, 0x24f
-    branchl r12, memset
-
-InitSettings:
+    lwz r4, MemcardData(r13)
     # Set Max OSD on No Memcard
     li r3, 1
-    lwz r4, MemcardData(r13)
     stb r3, OSDMaxWindows(r4)
     # Set Initial Page Number
-    li r3, 0x1
+    li r3, 1
     stb r3, CurrentEventPage(r4)
     # Enable Recommended OSDs
     li r3, 0
@@ -57,7 +27,14 @@ InitSettings:
     stw r3, OSDBitfield(r4)
 
     restore
-    blr
+
+    cmpwi r29, MEMCARD_NOSAVE
+    beq Original
+
+NoMemcard:
+    # No memcard available
+    # Exit memcard think and disable saving
+    branch r12, 0x801b01ac
 
 Original:
     cmpwi r29, 0
