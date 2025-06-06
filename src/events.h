3bceead38c680fe9ca9741757ffaf2b39af5c521
Nick Condron <ncondron42@gmail.com>
2025-06-06 01:55:28 -0400

    Remove unnecessary MatchData fields

--- src/events.h
+++ src/events.h
@@ -53,27 +53,19 @@ typedef struct EventMatchData //r8
 {
     unsigned int timer : 2;
     unsigned int matchType : 3;
-    unsigned int isDisableMusic : 1;
     unsigned int hideGo : 1;
     unsigned int hideReady : 1;
-    unsigned int isCreateHUD : 1;
-    unsigned int isDisablePause : 1;
-    unsigned int timerRunOnPause : 1;    // 0x01
-    unsigned int isHidePauseHUD : 1;     // 0x02
-    unsigned int isShowLRAStart : 1;     // 0x04
-    unsigned int isCheckForLRAStart : 1; // 0x08
-    unsigned int isShowZRetry : 1;       // 0x10
-    unsigned int isCheckForZRetry : 1;   // 0x20
-    unsigned int isShowAnalogStick : 1;  // 0x40
-    unsigned int isShowScore : 1;        // 0x80
-    unsigned int isRunStockLogic : 1;    // 0x20
-    unsigned int isDisableHit : 1;       // 0x20
+    unsigned int isCreateHUD : 1;       // Display Stocks/Percents/Timer
+    unsigned int timerRunOnPause : 1;
+    unsigned int isCheckForZRetry : 1;
+    unsigned int isShowScore : 1;
+    unsigned int isRunStockLogic : 1;
+    unsigned int isDisableHit : 1;
     unsigned int useKOCounter : 1;
     s8 playerKind;                    // -1 = use selected fighter
     s8 cpuKind;                       // -1 = no CPU
     s16 stage;                        // -1 = use selected stage
     unsigned int timerSeconds : 32;   // 0xFFFFFFFF
-    unsigned int timerSubSeconds : 8; // 0xFF
 } EventMatchData;
 enum CSSID {
     CSSID_DOCTOR_MARIO   = 1 << 0,
