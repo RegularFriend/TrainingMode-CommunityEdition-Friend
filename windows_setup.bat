4f201707581681cd7d49bc2d10bb21568b20fedb
Aitch <48610650+AlexanderHarrison@users.noreply.github.com>
2025-06-15 18:13:37 -0600

    oops, commited temp testing change

--- windows_setup.bat
+++ windows_setup.bat
@@ -4,7 +4,7 @@ setlocal
 REM set directory to location of this file. Needed when drag n dropping across directories.
 cd /d %~dp0
 
-if exist "C:/devkitPro/devkitPPC" (
+if not exist "C:/devkitPro/devkitPPC" (
     echo ERROR: devkitPro not found at "C:/devkitPro/devkitPPC"
     echo Please install devkitPro with the GameCube package
     goto end
