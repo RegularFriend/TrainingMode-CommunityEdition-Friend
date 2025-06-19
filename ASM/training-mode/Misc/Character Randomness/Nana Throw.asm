7daa1aa015779a2c22446fb6891256b748469a13
Nick Condron <ncondron42@gmail.com>
2025-06-19 01:25:55 -0400

    Improve Nana throw randomness asm
    The new code will allow nana to randomly pummel and not affect throws
    that are nonrandom based on stage position.

--- ASM/training-mode/Misc/Character Randomness/Nana Throw.asm
+++ ASM/training-mode/Misc/Character Randomness/Nana Throw.asm
@@ -1,20 +1,35 @@
-# To be inserted at 800dd368
+    # To be inserted at 800b6ff4 
 
-.include "Common.s"
+    .include "Common.s"
 
-# r4 = selected throw
-# r31 contains Fighter
+    lwz r3, 0x4(r29) # internal char id
+    cmpwi r3, 0xb    # Nana
+    bne Exit
+    CharRng_FetchSetting r3, CharRng_Setting_Nana_Throw, Exit
+    cmpwi r3, 0 # 0 = default setting
+    beq Exit
 
-# original line
-addi r4, r30, 0
-lwz r30, 0x4(r31) # internal char id
-cmpwi r30, 0xb
-bne- Exit # isn't Nana
+    lfs f31, -0x6F88(rtoc) # loads 0.25
+    fsubs f1, f1, f1       # f1 = 0.0
 
-CharRng_FetchSetting r5, CharRng_Setting_Nana_Throw, Exit
-cmpwi r5, 0 # 0 = default setting
-beq+ Exit
-li r4, 0xda # 1 - fthrow
-add r4, r4, r5
+    cmpwi r3, 1
+    beq Fthrow
+    cmpwi r3, 2
+    beq Bthrow
+    cmpwi r3, 3
+    beq Uthrow
+    cmpwi r3, 4
+    beq Dthrow
+
+    # The CPU AI throws randomly based on a random float. Each throw has a 0.25
+    # probablitity [0,.25)=up, [.25,.5)=down, [.5,.75)=fwd, [.75,1)=back
+Bthrow:
+    fadds f1, f1, f31
+Fthrow:
+    fadds f1, f1, f31
+Dthrow:
+    fadds f1, f1, f31
+Uthrow:
 
 Exit:
+    fmr f31, f1 # original line
