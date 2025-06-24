df7f40408887d3d3dfa1508611972b531f510787
Alex Harrison <alexander.harrison.r@gmail.com>
2025-06-24 15:08:08 -0600

    replace tabs with spaces

--- MexTK/include/hsd.h
+++ MexTK/include/hsd.h
@@ -271,38 +271,38 @@ struct HSD_Archive
 };
 
 typedef struct HSD_PadData {
-	PADStatus stat[4];
+    PADStatus stat[4];
 } HSD_PadData;
 
 typedef struct PadLibData {
-	u8 qnum;
-	u8 qread;
-	u8 qwrite;
-	u8 qcount;
-	u8 qtype;
-	HSD_PadData *queue;
-	s32 repeat_start;
-	s32 repeat_interval;
-	u8 adc_type;
-	s8 adc_th;
-	f32 adc_angle;
-	u8 clamp_stickType;
-	u8 clamp_stickShift;
-	s8 clamp_stickMax;
-	s8 clamp_stickMin;
-	u8 clamp_analogLRShift;
-	u8 clamp_analogLRMax;
-	u8 clamp_analogLRMin;
-	u8 clamp_analogABShift;
-	u8 clamp_analogABMax;
-	u8 clamp_analogABMin;
-	s8 scale_stick;
-	u8 scale_analogLR;
-	u8 scale_analogAB;
-	u8 cross_dir;
-	u8 reset_switch_status;
-	u8 reset_switch;
-	void *rumble_info;
+    u8 qnum;
+    u8 qread;
+    u8 qwrite;
+    u8 qcount;
+    u8 qtype;
+    HSD_PadData *queue;
+    s32 repeat_start;
+    s32 repeat_interval;
+    u8 adc_type;
+    s8 adc_th;
+    f32 adc_angle;
+    u8 clamp_stickType;
+    u8 clamp_stickShift;
+    s8 clamp_stickMax;
+    s8 clamp_stickMin;
+    u8 clamp_analogLRShift;
+    u8 clamp_analogLRMax;
+    u8 clamp_analogLRMin;
+    u8 clamp_analogABShift;
+    u8 clamp_analogABMax;
+    u8 clamp_analogABMin;
+    s8 scale_stick;
+    u8 scale_analogLR;
+    u8 scale_analogAB;
+    u8 cross_dir;
+    u8 reset_switch_status;
+    u8 reset_switch;
+    void *rumble_info;
 } PadLibData;
 
 /*** Static Variables ***/
